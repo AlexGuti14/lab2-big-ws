eb0ca956f34d0d00b6ae89c2176410afb95d773c
Alejandro Gutierrez <a.guti3@hotmail.com>
2019-11-03 12:00:43 +0100

    New test (en->ca, en->gl)

--- src/test/java/translator/service/TranslatorServiceTest.java
+++ src/test/java/translator/service/TranslatorServiceTest.java
@@ -20,9 +20,21 @@ public class TranslatorServiceTest {
   TranslatorService translatorService;
 
   @Test
-  public void translateTest() {
+  public void translateTest_en_to_es() {
     TranslatedText translatedText = translatorService.translate("en", "es", "This is a test of translation service");
     assertEquals("Esto es una prueba de servicio de traducción", translatedText.getTranslation());
+  }
+
+  @Test
+  public void translateTest_en_to_ca() {
+    TranslatedText translatedText = translatorService.translate("en", "ca", "Hello i am student of university");
+    assertEquals("Hola sóc estudiant de la universitat", translatedText.getTranslation());
+  }
+
+  @Test
+  public void translateTest_en_to_gl() {
+    TranslatedText translatedText = translatorService.translate("en", "gl", "My name is alex");
+    assertEquals("O meu nome é alex", translatedText.getTranslation());
   }
 
 }
